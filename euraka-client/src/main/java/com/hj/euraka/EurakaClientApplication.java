394a2dc4b672aec71695f20a9d61f39da96f28dd
qingjiesiyi <365896049@qq.com>
2019-04-24 17:43:42 +0800

    Initial commit

---
+++ euraka-client/src/main/java/com/hj/euraka/EurakaClientApplication.java
@@ -0,0 +1,15 @@
+package com.hj.euraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurakaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakaClientApplication.class, args);
+    }
+
+}
